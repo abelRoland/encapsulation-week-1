b4540309590467b18501932e114375c12ac76fac
AR. Jesus <abelroland@gmail.com>
2020-09-04 17:09:26 +0200

    Adding and Viewing User Stories This closes #2 and also this closes #3

--- src/handlers/toggle-completed.js
+++ src/handlers/toggle-completed.js
@@ -1,5 +1,31 @@
 'use strict';
 
+const addTodo = () => {
+  const input = document.getElementById('text-to-add').value;
+  initialState.setTodo(input);
+  console.log(initialState.todos)
+
+  // update UI from state
+
+  // re-render the average
+  const listContainer = document.getElementById('root');
+      while (listContainer.firstChild) {
+        listContainer.removeChild(listContainer.firstChild);
+      }
+      const renderList = renderTodos(initialState.todos);
+      document.getElementById('root').appendChild(renderList);
+      //listContainer.appendChild(renderList);
+      // log new view rendered from state
+      //logEntry.viewList = renderList;
+     };
+
+// render initial view and attach event listeners
+const displayNew = () => {
+  const todosView = renderTodos(app.state.todos);
+  todosView.addEventListener('change', toggleCompletedHandler); // event delegation!
+  document.getElementById('root').appendChild(todosView);
+
+}
 const toggleCompletedHandler = (event) => {
 
   // event delegation!
